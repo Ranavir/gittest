3032fa7afc791b07510d4504123c57339025d368
Ranavir Dash <ranavir.silicon@gmail.com>
2016-06-10 14:23:34 +0530

    B01 commit.

--- gittest/src/gittest/GitTestDemo.java
+++ gittest/src/gittest/GitTestDemo.java
@@ -5,8 +5,8 @@ public class GitTestDemo {
 		System.out.println("Ranavir Dash");
 		System.out.println("Added in 01");
 		System.out.println("Added in master");
-		System.out.println("Added in 01 01");
 		System.out.println("Added after stash in 01 2nd time.");
+		System.out.println("Added in 01 01");
 	}
 
 }
