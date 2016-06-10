1548ca68737c030c0c62c1a8dc72269002c89ca6
Ranavir Dash <ranavir.silicon@gmail.com>
2016-06-10 14:41:23 +0530

    01

--- gittest/src/gittest/GitTestDemo.java
+++ gittest/src/gittest/GitTestDemo.java
@@ -2,12 +2,7 @@ package gittest;
 
 public class GitTestDemo {
 	public static void main(String[] args) {
-		System.out.println("Ranavir Dash");
-		System.out.println("Added in 01");
-		System.out.println("Added in master");
-		System.out.println("Added after stash in 01 2nd time.");
-		System.out.println("Added in 01 01");
-		System.out.println("master 01");
+		System.out.println(01);
 	}
 
 }
