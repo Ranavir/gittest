2ae97bf55f33936280f29ef8766923550f64df62
Ranavir Dash <ranavir.silicon@gmail.com>
2016-06-10 13:10:14 +0530

    added 1st master.

--- gittest/src/gittest/GitTestDemo.java
+++ gittest/src/gittest/GitTestDemo.java
@@ -4,6 +4,7 @@ public class GitTestDemo {
 	public static void main(String[] args) {
 		System.out.println("Ranavir Dash");
 		System.out.println("Added in 01");
+		System.out.println("Added in master");
 	}
 
 }
